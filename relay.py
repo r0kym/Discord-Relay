370f2767e81f762dd5de2af5aede50087db69489
r0kym <vincentvanmuys@hotmail.com>
2022-08-21 00:29:24 +0200

    Limit message length to 1024 (max embed field length)

--- relay.py
+++ relay.py
@@ -17,7 +17,7 @@ class MyClient(discord.Client):
         if message.channel.id in SPY_CHANNELS and message.content:
             embed = discord.Embed(title=message.channel.guild.name)
             embed.set_author(name=message.author.display_name, icon_url=f'https://cdn.discordapp.com/avatars/{message.author.id}/{message.author.avatar}.webp')
-            embed.add_field(name=message.channel.name, value=message.content)
+            embed.add_field(name=message.channel.name, value=message.content[:1024])
             webhook = discord.Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=discord.RequestsWebhookAdapter())
 
             webhook.send(embed=embed)
