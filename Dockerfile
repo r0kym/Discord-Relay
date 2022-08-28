5cc25a49cfca680fa561aacc1449e7bdf530e3ed
r0kym <vincentvanmuys@hotmail.com>
2022-08-28 13:50:32 +0200

    Big bot improvement  - Relay messages embed  - Add user name after a mention  - Limit max message length

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM python:3
 
 WORKDIR /usr/src/app
 
+ENV PYTHONUNBUFFERED=1
+ENV PYTHONDONTWRITEBYTECODE=1
+
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
--- relay.py
+++ relay.py
@@ -1,26 +1,75 @@
 import discord
 import os
 import json
-
+import re
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
 SPY_CHANNELS = json.loads(os.environ.get("SPY_CHANNELS"))
-WEBHOOK_ID = os.environ.get("WEBHOOK_ID")
+WEBHOOK_ID = int(os.environ.get("WEBHOOK_ID"))
 WEBHOOK_TOKEN = os.environ.get("WEBHOOK_TOKEN")
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print(f"Logged on as {self.user}, tracking: ")
+        for channel_id in SPY_CHANNELS:
+            print(f" - {self.get_channel(channel_id)}")
 
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
+        webhook = discord.Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=discord.RequestsWebhookAdapter())
+
         if message.channel.id in SPY_CHANNELS and message.content:
             embed = discord.Embed(title=message.channel.guild.name)
-            embed.set_author(name=message.author.display_name, icon_url=f'https://cdn.discordapp.com/avatars/{message.author.id}/{message.author.avatar}.webp')
-            embed.add_field(name=message.channel.name, value=message.content[:1024])
-            webhook = discord.Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=discord.RequestsWebhookAdapter())
+            embed.set_author(name=message.author.display_name,
+                             icon_url=f'https://cdn.discordapp.com/avatars/{message.author.id}/{message.author.avatar}.webp')
+            embed.add_field(name=message.channel.name, value=self.reformat_message(message))
 
-            webhook.send(embed=embed)
+            webhook.send(embeds=[embed] + message.embeds[:9])
+
+    def reformat_message(self, message: discord.Message) -> str:
+        """
+        Reformat the message to improve mentions and make sure it's short enough (can't be more than 1024 chars)
+        :param message: discord message to reformat
+        :return: message content reworked
+        """
+
+        message_content = message.content
+        user_mentions = re.finditer(r'<@\d{18}>', message_content)
+        offset = 0
+        for mention in user_mentions:
+            user_id = 0
+            try:
+                user_id = int(mention.group(0)[2:20])
+            except ValueError:
+                pass
+
+            if user_id:
+                username = self.member_or_user_name_from_id(user_id, message.guild)
+                message_content = message_content[:mention.span()[1]+offset] + f"({username})" + message_content[mention.span()[1]+offset:]
+                offset += len(username) + 2
+
+        if len(message_content) > 1024:
+            message_content = message_content[:1021] + "..."
+
+        return message_content
+
+    def member_or_user_name_from_id(self, user_id: int, guild: discord.Guild) -> str:
+        """
+        Will return the guild specific nick of the user if applicable or the standard username
+
+        :param user_id: id of the discord user to search
+        :param guild: Guild to check for specific nick. Default: None
+        :return: String, name
+        """
+
+        if guild and (member := guild.get_member(user_id)) is not None:
+            if (nick := member.nick) is not None:
+                return nick
+
+        if user := self.get_user(user_id):
+            return user.name
+
+        return "Name not found"
 
 
 client = MyClient()
